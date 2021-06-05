d87e3152bf9996c68689db38452da4424fc9e61d
zongziren <zsr1341864378@mail.ustc.edu.cn>
2021-06-05 15:01:17 -0700

    cargo new zrsh

---
+++ Lab2/zrsh/target/rls/debug/deps/zrsh-5e60d6cd24c9a689.d
@@ -0,0 +1,5 @@
+/home/zsr/study/OSH-2021-Labs/Lab2/zrsh/target/rls/debug/deps/zrsh-5e60d6cd24c9a689.rmeta: src/main.rs
+
+/home/zsr/study/OSH-2021-Labs/Lab2/zrsh/target/rls/debug/deps/zrsh-5e60d6cd24c9a689.d: src/main.rs
+
+src/main.rs:
---
+++ Lab2/zrsh/target/rls/debug/deps/zrsh-ec0d5f315ee5c4ab.d
@@ -0,0 +1,5 @@
+/home/zsr/study/OSH-2021-Labs/Lab2/zrsh/target/rls/debug/deps/zrsh-ec0d5f315ee5c4ab.rmeta: src/main.rs
+
+/home/zsr/study/OSH-2021-Labs/Lab2/zrsh/target/rls/debug/deps/zrsh-ec0d5f315ee5c4ab.d: src/main.rs
+
+src/main.rs:
